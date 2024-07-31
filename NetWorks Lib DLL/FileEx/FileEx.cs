a296bcb5b138c28f3218ba176c542ce3b223007e
Nov <28666422+EleventhNov@users.noreply.github.com>
2024-07-30 22:21:18 -0500

    Create a simpler FileEx API

---
+++ NetWorks Lib DLL/FileEx/FileEx.cs
@@ -0,0 +1,78 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using NetWorks.FileEx;
+using NetWorks.Network;
+using NetWorks.Security;
+
+namespace NetWorks_Lib_DLL.FileEx
+{
+    /// <summary>
+    /// The FileEx class provides methods for easily sharing large amounts of data, 
+    /// with encryption supported.
+    /// </summary>
+    public static class FileEx
+    {
+
+        /// <summary>
+        /// Send all of the data in the stream to the specified endpoint.
+        /// </summary>
+        /// <param name="dataStream">The stream containing all the data to be sent.</param>
+        /// <param name="endPoint">The remote endpoint to deliver the data to.</param>
+        public static void SendStream(Stream dataStream, IPEndPoint endPoint)
+        {
+            using TcpClient tcpClient = new();
+            tcpClient.Connect(endPoint);
+            Stream outputStream = tcpClient.GetStream();
+            dataStream.CopyTo(outputStream);
+        }
+
+        /// <summary>
+        /// Send all of the data in the stream to the specified endpoint,
+        /// encrypted using the provided public key.
+        /// </summary>
+        /// <param name="dataStream">The stream containing all the data to be sent.</param>
+        /// <param name="endPoint">The remote endpoint to deliver the data to.</param>
+        /// <param name="publicKey">The key to encrypt the data with.</param>
+        public static void SendStreamEncrypted(Stream dataStream, IPEndPoint endPoint, SecurityKey publicKey)
+        {
+            using TcpClient tcpClient = new();
+            tcpClient.Connect(endPoint);
+            Stream outputStream = tcpClient.GetStream();
+            SecureDataSender sender = new(outputStream, publicKey);
+            sender.SendStream(dataStream);
+        }
+
+        /// <summary>
+        /// Read a single file from the endpoint into the output stream.
+        /// </summary>
+        /// <param name="outputStream">The stream where the received data will be written to.</param>
+        /// <param name="endPoint">The remote endpoint to receive data from.</param>
+        public static void ReceiveStream(Stream outputStream, IPEndPoint endPoint)
+        {
+            TcpListener tcpListener = new(endPoint);
+            tcpListener.Start();
+            TcpClient tcpClient = tcpListener.AcceptTcpClient();
+            Stream inputStream = tcpClient.GetStream();
+            inputStream.CopyTo(outputStream);
+            tcpListener.Stop();
+        }
+
+        /// <summary>
+        /// Read a single file from the endpoint into the output stream,
+        /// decrypting the data using the provided private key.
+        /// </summary>
+        /// <param name="outputStream">The stream where the received data will be written to.</param>
+        /// <param name="endPoint">The remote endpoint to receive data from.</param>
+        /// <param name="privateKey">The key to decrypt the data with.</param>
+        public static void ReceiveStreamEncrypted(Stream outputStream, IPEndPoint endPoint, SecurityKey privateKey)
+        {
+            TcpListener tcpListener = new(endPoint);
+            tcpListener.Start();
+            TcpClient tcpClient = tcpListener.AcceptTcpClient();
+            Stream inputStream = tcpClient.GetStream();
+            SecureDataReceiver receiver = new(inputStream, privateKey);
+            receiver.ReceiveStream(outputStream);
+            tcpListener.Stop();
+        }
+    }
+}
---
+++ NetWorks Library/Examples/SendFileViaFileEx.cs
@@ -0,0 +1,69 @@
+﻿using System.Net;
+using NetWorks.FileEx;
+using NetWorks.Security;
+using NetWorks_Lib_DLL.FileEx;
+using NetWorks_Library.FileGenerator;
+
+namespace NetWorks_Library.Examples
+{
+    /// <summary>
+    /// Showcases how to send a file via FileEx, both with no encryption and with encryption.
+    /// </summary>
+    internal class SendFileViaFileEx
+    {
+        /// <summary>
+        /// The endpoint defines over what IP & port the data should be transmitted.
+        /// We will use localhost for the purposes of this example.
+        /// </summary>
+        readonly IPEndPoint endPoint = new(IPAddress.Parse("127.0.0.1"), 9999);
+
+        /// <summary>
+        /// To encrypt the exchanged data, we make use of a SecurityKeypair.
+        /// This class stores a matching set of public and private keys, which
+        /// are used by the sender and receiver to encrypt and decrypt the
+        /// exchanged data, respectively.
+        /// </summary>
+        readonly SecurityKeypair exchangeKeypair = new();
+
+        public void Run()
+        {
+            /// For this example we will send garbage, but in your case
+            /// you can send any meaningful data (if you have any.)
+            byte[] data = UselessFileGenerator.RandomBytes(16);
+            Console.WriteLine($"The data to be sent is: {BitConverter.ToString(data)}");
+
+            var task = Task.Run(RunReceive);
+            
+            /// Despite the name of the class, FileEx actually works with streams,
+            /// not just files. You can send any stream you have over FileEx,
+            /// as long as the end of the stream matches the end of the data.
+            using var dataStream = new MemoryStream(data);
+
+            Console.WriteLine("Sending a non-encrypted file over FileEx.");
+            dataStream.Seek(0, SeekOrigin.Begin);
+            FileEx.SendStream(dataStream, endPoint);
+
+            Console.WriteLine("Sending an encrypted file over FileEx.");
+            dataStream.Seek(0, SeekOrigin.Begin);
+            FileEx.SendStreamEncrypted(dataStream, endPoint, exchangeKeypair.PublicKey);
+
+            task.Wait();
+            Console.WriteLine("The files have been successfully sent.");
+        }
+        
+        private void RunReceive()
+        {
+            using MemoryStream outputStream = new();
+
+            Console.WriteLine("Receiving a non-encrypted file over FileEx.");
+            outputStream.SetLength(0);
+            FileEx.ReceiveStream(outputStream, endPoint);
+            Console.WriteLine($"Received data: {BitConverter.ToString(outputStream.ToArray())}");
+
+            Console.WriteLine("Receiving an encrypted file over FileEx.");
+            outputStream.SetLength(0);
+            FileEx.ReceiveStreamEncrypted(outputStream, endPoint, exchangeKeypair.PrivateKey);
+            Console.WriteLine($"Received data: {BitConverter.ToString(outputStream.ToArray())}");
+        }
+    }
+}
